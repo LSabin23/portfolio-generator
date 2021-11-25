a91429b9203fd1a56b1014b3920dd62f70e0a32c
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-25 12:58:33 -0500

    added function to generate html page with fs Node.js module

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+
 const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs)
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem))
-}
+const [name, github] = profileDataArgs
 
-printProfileData(profileDataArgs)
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  // creates an exception and stops the execution of the remaining code
+  if (err) throw new Error(err)
+
+  console.log('Portfolio complete! Check out index.html to see the output.')
+})
