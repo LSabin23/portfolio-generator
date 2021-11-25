52b95908ee250bac970c8507d877f71738f32859
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-25 16:08:27 -0500

    added inquirer and a prompt for users

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const inquirer = require('inquirer')
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
+
+/*
+commenting out this code since we don't need it for the rest of this lesson
+
 const fs = require('fs')
 const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+const pageHTML = generatePage(name, github)
 
-const [name, github] = profileDataArgs
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   // creates an exception and stops the execution of the remaining code
-  if (err) throw new Error(err)
+  if (err) throw err
 
   console.log('Portfolio complete! Check out index.html to see the output.')
 })
+*/
